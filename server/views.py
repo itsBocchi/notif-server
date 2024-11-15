8aba343dadcbda694a36779c181724980e152c6d
itsbocchi <cristian.gonzalezjo@sansano.usm.cl>
2024-11-15 00:17:48 -0300

    pasar weas

--- server/views.py
+++ server/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 import firebase_admin
 from firebase_admin import auth
 import json
+import geopy.distance
 
 # Asegúrate de importar la configuración de Firebase
 import firebase_config
@@ -44,3 +45,29 @@ def firebase_config(request):
         "appId": settings.FIREBASE_APP_ID,
     }
     return JsonResponse(config)
+
+def get_distance_coords(coords1, coords2):
+    return geopy.distance.distance(coords1, coords2).km
+
+# Recuperar fcm de de cada usuario cuya locacion esta a menos de 1km del la coordenada 0,0 de la 
+# coleccion usuarios
+def get_nearby_users(document_id):
+    users = []
+    for user in firebase_config.db.collection('usuarios').stream():
+        user_data = user.to_dict()
+        user_coords = (user_data['latitude'], user_data['longitude'])
+        # Obtener la alerta con el document_id
+        alert = firebase_config.db.collection('alertas').document(document_id).get()
+        if alert.exists:
+            alert_data = alert.to_dict()
+            evacuation_points = alert_data.get('puntos_evacuacion', [])
+            evacuation_radii = alert_data.get('radios_evacuacion', [])
+
+            # Calcular la distancia entre las coordenadas del usuario y los puntos de evacuación
+            for point, radius in zip(evacuation_points, evacuation_radii):
+                point_coords = (point['latitude'], point['longitude'])
+                distance = get_distance_coords(user_coords, point_coords)
+                if distance <= radius:
+                    users.append(user_data['fcmToken'])
+                    break
+    return users
